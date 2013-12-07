4860959c2b4b8a77f081b0697dec37f9aa746ac7
Ricardo Mendes <ricardo.mendes@ndrive.com>
2013-12-07 01:23:24 +0000

    Everything and the kitchen sink.

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-jet-vermillion-generator
-========================
+# jet-vermillion-generator [![Build Status](https://secure.travis-ci.org/locks/jet-vermillion-generator.png?branch=master)](http://travis-ci.org/locks/jet-vermillion-generator)
 
 Generates codes to unlock Jet Vermillion in F-Zero Maximum Velocity
+
+## Getting Started
+Install the module with: `npm install jet-vermillion-generator`
+
+```javascript
+var jet-vermillion-generator = require('jet-vermillion-generator');
+jet-vermillion-generator.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Ricardo Mendes. Licensed under the MIT license.
---
+++ test/jet-vermillion-generator_test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var jet_vermillion_generator = require('../lib/jet-vermillion-generator.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.jet_vermillion_generator = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'generates code properly': function(test) {
+    test.equal(jet_vermillion_generator.generate('LOCKS'), 'W-XX9NV8A+30', 'should be W-XX9NV8A+30.');
+
+    test.done();
+  },
+  'generates multiple codes properly': function(test) {
+    test.equal(jet_vermillion_generator.generate('LOCKS'), 'W-XX9NV8A+30', 'should be W-XX9NV8A+30.');
+    test.equal(jet_vermillion_generator.generate('RENAN'), 'M-AFY9V8A-30', 'should be M-AFY9V8A-30.');
+
+    test.done();
+  }
+};
